15de4dfa70e75a5cb75c6885c975abb27932790a
EC2 Default User <koichan1996@icloud.com>
2023-01-10 10:31:11 +0000

    ff

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -47,7 +47,7 @@ before_action :ensure_correct_user, only: [:update,:edit]
   private
 
   def book_params
-    params.require(:book).permit(:title,:body)
+    params.require(:book).permit(:title,:body,:rating)
   end
   
   def ensure_correct_user
